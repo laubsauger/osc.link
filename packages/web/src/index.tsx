242ea095d9fa172e26d840d7cddbcdf60c0b6e5e
wills <will@simons.dev>
2024-08-20 13:23:26 -0700

    migrate web package to use vite for building.

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { StoreProvider } from './contexts/storeContext';
-import SensorsProvider from './contexts/sensorsContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./contexts/storeContext";
+import SensorsProvider from "./contexts/sensorsContext";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-dark-5/dist/css/bootstrap-night.css";
-import './index.scss';
-import App from './components/App';
-import {RootStore} from "./stores/rootStore";
+import "./index.scss";
+import App from "./components/App";
+import { RootStore } from "./stores/rootStore";
 // @ts-ignore
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById('root');
-
-// @ts-ignore: Unreachable code error
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <SensorsProvider multiplier={3} useGravity={false}>
-      <StoreProvider store={new RootStore()}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StoreProvider>
-    </SensorsProvider>
-  </React.StrictMode>
-);
+export default function Index() {
+  return (
+    <React.StrictMode>
+      <SensorsProvider multiplier={3} useGravity={false}>
+        <StoreProvider store={new RootStore()}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </StoreProvider>
+      </SensorsProvider>
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
