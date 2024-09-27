79ec9378fc163081fd25f9e0e8c2666216fb79da
wills <will@simons.dev>
2024-09-26 22:41:57 -0700

    add dates to join

--- packages/web/src/components/App/Pages/Join.tsx
+++ packages/web/src/components/App/Pages/Join.tsx
@@ -61,154 +61,167 @@ const Join: React.FC<JoinProps> = (props) => {
         {isLoadingInstances && <LoadingSpinner size="small" />}
         {!isLoadingInstances && instances.length ? (
           <Accordion>
-            {instances.map((instance) => (
-              <Accordion.Item key={instance.id} eventKey={String(instance.id)}>
-                <Accordion.Header>{instance.name}</Accordion.Header>
-                <Accordion.Body>
-                  <Row className="mb-3">
-                    <Col lg={6} md={12}>
-                      <h6 className="text-muted">Instance Link</h6>
-                      <a href={`/session/${instance.id}`}>
-                        {`${window.location.origin}/session/${instance.id}`}
-                      </a>
-                    </Col>
-                    <Col lg={3} xs={6} className="mb-3">
-                      <h6 className="text-muted">Edit</h6>
-                      <div>
-                        <Link
-                          to={`${window.location.origin}/session/edit/${instance.id}`}
-                        >
-                          Edit Instance
-                        </Link>
-                      </div>
-                    </Col>
-                    <Col lg={3} xs={6} className="mb-3">
-                      <h6 className="text-muted">Edit</h6>
-                      <div>
-                        <Card.Link
-                          style={{ cursor: "pointer" }}
-                          onClick={() => props.deleteInstance(instance)}
-                        >
-                          Delete Instance
-                        </Card.Link>
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col lg={6} md={12} className="mb-3">
-                      <h6 className="text-muted">Description</h6>
-                      <div>{instance.description}</div>
-                    </Col>
-                    <Col lg={3} xs={6} className="mb-3">
-                      <h6 className="text-muted">Settings</h6>
-                      <div>
+            {instances
+              .sort(
+                (a, b) =>
+                  new Date(a.createdAt).getTime() -
+                  new Date(b.createdAt).getTime()
+              )
+              .map((instance) => (
+                <Accordion.Item
+                  key={instance.id}
+                  eventKey={String(instance.id)}
+                >
+                  <Accordion.Header>{instance.name}</Accordion.Header>
+                  <Accordion.Body>
+                    <Row className="mb-3">
+                      <Col lg={6} md={12}>
+                        <h6 className="text-muted">Instance Link</h6>
+                        <a href={`/session/${instance.id}`}>
+                          {`${window.location.origin}/session/${instance.id}`}
+                        </a>
+                      </Col>
+                      <Col lg={3} xs={6} className="mb-3">
+                        <h6 className="text-muted">Edit</h6>
                         <div>
-                          <Badge bg="secondary">
-                            slots [ {instance.settings.slots} ]
-                          </Badge>
+                          <Link
+                            to={`${window.location.origin}/session/edit/${instance.id}`}
+                          >
+                            Edit Instance
+                          </Link>
                         </div>
-                        {instance.settings.slotPick ? (
+                      </Col>
+                      <Col lg={3} xs={6} className="mb-3">
+                        <h6 className="text-muted">Edit</h6>
+                        <div>
+                          <Card.Link
+                            style={{ cursor: "pointer" }}
+                            onClick={() => props.deleteInstance(instance)}
+                          >
+                            Delete Instance
+                          </Card.Link>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg={6} md={12} className="mb-3">
+                        <h6 className="text-muted">Description</h6>
+                        <div>{instance.description}</div>
+                      </Col>
+                      <Col lg={3} xs={6} className="mb-3">
+                        <h6 className="text-muted">Settings</h6>
+                        <div>
                           <div>
-                            <Badge bg="secondary">slotPick</Badge>
+                            <Badge bg="secondary">
+                              slots [ {instance.settings.slots} ]
+                            </Badge>
                           </div>
-                        ) : null}
-                        {instance.settings.randomPick ? (
-                          <div>
-                            <Badge bg="secondary">randomPick</Badge>
-                          </div>
-                        ) : null}
-                        {instance.settings.sequentialPick ? (
-                          <div>
-                            <Badge bg="secondary">sequentialPick</Badge>
-                          </div>
-                        ) : null}
-                      </div>
-                    </Col>
-                    <Col lg={3} xs={6}>
-                      <h6 className="text-muted">Controls</h6>
-                      <div className="small overflow-x-hidden">
-                        {instance?.settings?.controls ? (
-                          Object.entries(instance.settings.controls)
-                            .filter(([key, val]) => !!val)
-                            .map(([key, val]) => (
-                              <div key={key}>
-                                <div>
-                                  <div className="bg-black ps-2 rounded-top">
-                                    {key}
-                                  </div>
-                                  <div className="bg-black ps-3 text-muted">
-                                    {val.map((v) => (
-                                      <div key={v.id}>
-                                        {v.id}:{v.type}
-                                      </div>
-                                    ))}
+                          {instance.settings.slotPick ? (
+                            <div>
+                              <Badge bg="secondary">slotPick</Badge>
+                            </div>
+                          ) : null}
+                          {instance.settings.randomPick ? (
+                            <div>
+                              <Badge bg="secondary">randomPick</Badge>
+                            </div>
+                          ) : null}
+                          {instance.settings.sequentialPick ? (
+                            <div>
+                              <Badge bg="secondary">sequentialPick</Badge>
+                            </div>
+                          ) : null}
+                        </div>
+                      </Col>
+                      <Col lg={3} xs={6}>
+                        <h6 className="text-muted">Controls</h6>
+                        <div className="small overflow-x-hidden">
+                          {instance?.settings?.controls ? (
+                            Object.entries(instance.settings.controls)
+                              .filter(([key, val]) => !!val)
+                              .map(([key, val]) => (
+                                <div key={key}>
+                                  <div>
+                                    <div className="bg-black ps-2 rounded-top">
+                                      {key}
+                                    </div>
+                                    <div className="bg-black ps-3 text-muted">
+                                      {val.map((v) => (
+                                        <div key={v.id}>
+                                          {v.id}:{v.type}
+                                        </div>
+                                      ))}
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                            ))
-                        ) : (
-                          <div>No controls available</div>
+                              ))
+                          ) : (
+                            <div>No controls available</div>
+                          )}
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg={6} md={12} className="mb-3">
+                        <h6 className="text-muted">Date Created</h6>
+                        <div>
+                          {new Date(instance.createdAt).toLocaleString()}
+                        </div>
+                      </Col>
+                      <Col lg={3} xs={6} className="mb-3">
+                        <h6 className="text-muted">Last Updated</h6>
+                        <div>
+                          {new Date(instance.updatedAt).toLocaleString()}
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div>
+                        <hr />
+                        {instance.settings.slotPick && (
+                          <>
+                            <div className="mt-4 text-center">Choose Slot</div>
+                            <div
+                              className="btn-group d-flex flex-wrap"
+                              role="group"
+                              aria-label="Basic outlined example"
+                            >
+                              {SlotButtons(instance)}
+                            </div>
+                          </>
+                        )}
+                        {instance.settings.randomPick && (
+                          <>
+                            <div className="mt-4 text-center">
+                              Take a randomly selected slot
+                            </div>
+                            <LinkButton
+                              path={`/session/${instance.id}/0`}
+                              label={"Join"}
+                              variant={"outline-info"}
+                            />
+                          </>
+                        )}
+                        {instance.settings.sequentialPick && (
+                          <>
+                            <div className="mt-4 text-center">
+                              Take next free slot
+                            </div>
+                            <LinkButton
+                              path={`/session/${instance.id}/0`}
+                              label={"Join"}
+                              variant={"outline-info"}
+                            />
+                          </>
                         )}
                       </div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col lg={6} md={12} className="mb-3">
-                      <h6 className="text-muted">Date Created</h6>
-                      <div>{new Date(instance.createdAt).toLocaleString()}</div>
-                    </Col>
-                    <Col lg={3} xs={6} className="mb-3">
-                      <h6 className="text-muted">Last Updated</h6>
-                      <div>{new Date(instance.updatedAt).toLocaleString()}</div>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <div>
-                      <hr />
-                      {instance.settings.slotPick && (
-                        <>
-                          <div className="mt-4 text-center">Choose Slot</div>
-                          <div
-                            className="btn-group d-flex flex-wrap"
-                            role="group"
-                            aria-label="Basic outlined example"
-                          >
-                            {SlotButtons(instance)}
-                          </div>
-                        </>
-                      )}
-                      {instance.settings.randomPick && (
-                        <>
-                          <div className="mt-4 text-center">
-                            Take a randomly selected slot
-                          </div>
-                          <LinkButton
-                            path={`/session/${instance.id}/0`}
-                            label={"Join"}
-                            variant={"outline-info"}
-                          />
-                        </>
-                      )}
-                      {instance.settings.sequentialPick && (
-                        <>
-                          <div className="mt-4 text-center">
-                            Take next free slot
-                          </div>
-                          <LinkButton
-                            path={`/session/${instance.id}/0`}
-                            label={"Join"}
-                            variant={"outline-info"}
-                          />
-                        </>
-                      )}
-                    </div>
-                  </Row>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))}
+                    </Row>
+                  </Accordion.Body>
+                </Accordion.Item>
+              ))}
           </Accordion>
         ) : (
-          <div>No sessions found.</div>
+          <></>
         )}
       </div>
     </Col>
