dd563412d09df5caa65cbf9bca25ddafabb830eb
wills <bigredwill@gmail.com>
2024-10-28 13:18:17 -0700

    code cleanup: separate socket-server setup in to own file.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,17 +2,10 @@ import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
 import http from "http";
 import cors from "cors";
-import {
-  onOscJoinRequest,
-  onOscHostMessage,
-  onOscCtrlMessage,
-  onDisconnect,
-  onUserJoinRequest,
-} from "./socket";
 import sequelize from "./database";
 import instanceRoutes from "./routes/instances";
 import defineAssociations from "./models/associations";
-import { Socket, Server } from "socket.io";
+import createSocketServer from "./socket-server";
 
 /**
  * This file sets up
@@ -54,65 +47,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use(express.json());
 app.use("/api/instances", instanceRoutes);
 
-export default app;
-
-const server = http.createServer(app).listen(port, async () => {
+export const server = http.createServer(app).listen(port, async () => {
   console.log("listening on " + port);
+  // setup DB
   defineAssociations();
   await sequelize.sync();
 });
 
-let io = new Server({
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-}).listen(server);
+createSocketServer(server, allowedOrigins);
 
-/**
- * Register all socket event handlers. There should only be one "connection" handler.
- */
-io.on("connection", (socket: Socket) => {
-  // assignedClientSlotIndex is tied to the socket state.
-  socket.on(
-    "OSC_JOIN_REQUEST",
-    async (data) => await onOscJoinRequest({ socket, data, io })
-  );
-  socket.on(
-    "OSC_HOST_MESSAGE",
-    async (data) =>
-      await onOscHostMessage({
-        socket,
-        data,
-        io,
-      })
-  );
-
-  socket.on(
-    "OSC_CTRL_MESSAGE",
-    async (data) =>
-      await onOscCtrlMessage({ socket, data, io })
-  );
-
-  socket.on("USER_JOIN_REQUEST", async (data) => {
-    await onUserJoinRequest({
-      socket,
-      data,
-      io,
-    });
-  });
-
-  socket.on(
-    "disconnect",
-    async () => await onDisconnect({ socket, io })
-  );
-
-  socket.on("connect_failed", (err) => {
-    console.log("connect failed!", err);
-  });
-
-  socket.on("error", (err) => {
-    console.log("there was an error on the connection!", err);
-  });
-});
+export default app;
